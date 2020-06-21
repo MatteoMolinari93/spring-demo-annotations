4ec3881d39dd4ac8cfe1f96d43f8e82deb19e0bd
Matteo Molinari <matteomolinari93@gmail.com>
2020-06-21 10:18:42 +0200

    Bean lifecycle method annotations

--- src/com/molim/springdemo/TennisCoach.java
+++ src/com/molim/springdemo/TennisCoach.java
@@ -1,12 +1,14 @@
 package com.molim.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
+//@Scope("prototype")
 public class TennisCoach implements Coach {
 	
 	@Autowired
@@ -29,6 +31,16 @@ public class TennisCoach implements Coach {
 
 	public String getDailyFortune() {
 		return this.fortuneService.getFortune();
+	}
+	
+	@PostConstruct
+	public void postInit() {
+		System.out.println("TennisCoach: postInit");
+	}
+	
+	@PreDestroy
+	public void beforeDestroy() {
+		System.out.println("TennisCoach: beforeDestroy");
 	}
 	
 }
