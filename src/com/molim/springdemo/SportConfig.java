9310e036035299c2c0160c2e228ee24479b0bf8e
Matteo Molinari <matteomolinari93@gmail.com>
2020-06-22 18:22:39 +0200

    Java code configuration

---
+++ src/com/molim/springdemo/SportConfig.java
@@ -0,0 +1,10 @@
+package com.molim.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.molim.springdemo")
+public class SportConfig {
+
+}
